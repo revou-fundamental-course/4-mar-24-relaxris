a8fb11de9588439fbb78bba0642ea135a84673e6
relaxris <162872308+relaxris@users.noreply.github.com>
2024-03-14 17:42:55 +0700

    init commit

--- js/script.js
+++ js/script.js
@@ -19,10 +19,10 @@ function hitung(){
     if ( (BMIfix) < 18.50 ) {
         result = "Kekurangan Berat Badan";
     }
-    else if ( (BMIfix) < 18.50 && (BMIfix) < 19.00 ) {
+    else if ( (BMIfix) > 18.50 && (BMIfix) < 19.00 ) {
         result = "Berat Badan Ideal";
     }
-    else if ( (BMIfix) < 25.00 && (BMIfix) < 29.90 ) {
+    else if ( (BMIfix) > 25.00 && (BMIfix) < 29.90 ) {
         result = "Kelebihan Berat Badan";
     }
     else if ((BMIfix) >= 30.00) {
