c8f6158ac120e66d8cccd6386b1e0950d1f119d5
relaxris <162872308+relaxris@users.noreply.github.com>
2024-03-14 09:29:50 +0700

    init commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 function hitung(){
     let berat = document.getElementById("beratbadan").value;
     let tinggi = document.getElementById("tinggibadan").value;
+    let umur = document.getElementById("usia").value;
 
     let BMI = ( parseFloat(berat) / (parseFloat(tinggi) * parseFloat(tinggi)) );
 
@@ -8,6 +9,20 @@ function hitung(){
 
     if (berat == ''){
         alert ('Harap Isi Berat Badan Anda Terlebih Dahulu')
+    }
+    else {
+        console.log('Sudah Terisi')
+    }
+
+    if (tinggi == ''){
+        alert ('Harap Isi Tinggi Badan Anda Terlebih Dahulu')
+    }
+    else {
+        console.log('Sudah Terisi')
+    }
+
+    if (umur == ''){
+        alert ('Harap Isi Usia Anda Terlebih Dahulu')
     }
     else {
         console.log('Sudah Terisi')
